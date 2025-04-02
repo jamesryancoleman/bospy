8c2a9e8f7d3360610a81115212040fd11be07854
James Coleman <jamesryancoleman@gmail.com>
2025-04-01 20:07:05 -0400

    able to Get and LoadInput

--- src/bospy/run.py
+++ src/bospy/run.py
@@ -1,6 +1,8 @@
 from bospy import common_pb2_grpc, common_pb2
+from typing import Any
 import grpc
 import os
+import re
 
 envVars:dict[str,str]
 args:list[str] = []
@@ -8,7 +10,47 @@ kwargs:dict[str, str] = {}
 
 SCHEDULER_ADDR = os.environ.get('SCHEDULER_ADDR', "localhost:2824")
 
+# values will be set by the Scheduler and are constant for lifetime of a container
+TXN = os.environ.get('TXN_ID', 0)
+FLOW = os.environ.get('FLOW_ID', 0)
+NODE = os.environ.get('NODE_ID', 0)
+
+DEFAULT_TOKEN = "000000000000"
+
+# used write output from this transaction
+WRITE_TOKEN = os.environ.get('WRITE_TOKEN', DEFAULT_TOKEN)
+
+# used to load output from the previous instantiations of this FLOW
+READ_TOKEN = os.environ.get('READ_TOKEN', DEFAULT_TOKEN)
+
+positionRe = re.compile('^\\$(?P<position>[0-9]+)$')
+
 # client calls
+def Get(keys:list[str], infer_type=True, token:str=None) -> dict[str,Any]:
+    if token is None:
+        token = WRITE_TOKEN
+
+    response: common_pb2.GetResponse
+    with grpc.insecure_channel(SCHEDULER_ADDR) as channel:
+        header = common_pb2.Header(Src="python_client", Dst=SCHEDULER_ADDR, SessionToken=token)
+        stub = common_pb2_grpc.ScheduleStub(channel)
+        response = stub.Get(common_pb2.GetRequest(
+            Header=header,
+            Keys=keys,
+        ))
+        if response.Error > 0:
+            print("error:", response.Error, ", errorMsg:", response.ErrorMsg)
+
+    values:dict={}
+    for p in response.Pairs:
+        v:int|float|bool|str
+        if infer_type:
+            v = InferType(p.Value)
+        values[p.Key] = v
+
+    return values
+
+
 def Run(image:str, *args, envVars:dict[str, str]=None, **kwargs) -> common_pb2.RunResponse:
     response: common_pb2.RunResponse
     with grpc.insecure_channel(SCHEDULER_ADDR) as channel:
@@ -25,6 +67,15 @@ def Run(image:str, *args, envVars:dict[str, str]=None, **kwargs) -> common_pb2.R
     return response
 
 def Return(*_args, **_kwargs) -> common_pb2.SetResponse:
+    """ Return exposes all positional and keywork arguments provided to shared
+        memory in the BOS.
+
+        Exposing variables requires a valid transaction number and session 
+        token. These are typically set automatically by the BOS Scheduler when
+        it starts the container but these may be overwritten with:
+            kwarg['txn_id'] = TXN_ID
+            kwarg['session_token'] = SESSION_TOKEN
+    """
     pairs:list[common_pb2.SetPair] = []
     hash_key = _kwargs.get("__key__")
     if hash_key is not None:
@@ -36,8 +87,9 @@ def Return(*_args, **_kwargs) -> common_pb2.SetResponse:
     for k, v in _kwargs.items():
         pairs.append(common_pb2.SetPair(Key=k, Value=str(v)))
     
-    txn_id = int(kwargs.get('txn_id'), 0)
-    session_token = kwargs.get('session_token')
+    # the default txn_id of 0 and token of 000000000000 will succeed
+    txn_id = int(kwargs.get('txn_id', 0))
+    session_token = kwargs.get('session_token', "000000000000")
     print("Return - txn: {}, session_id: {}".format(txn_id, session_token))
     header = common_pb2.Header(
                 TxnId=txn_id,
@@ -60,23 +112,45 @@ def Return(*_args, **_kwargs) -> common_pb2.SetResponse:
     return response
 
 
-def LoadInput(previous_session_token:str|list[str]) -> dict[str,str]:
+def LoadInput(last_call:bool=False, node:int=-1) -> dict[str,str]:
     """ Load results is used to get the output of a previous container execution.
-        A session token is uniquely defined for a given transaction, flow, and node.
         
-        If a new transaction wants to load results from a previous call to the
-        same flow the Scheduler must have retained the session details and passed 
-        them to the next invocation of that flow.
+        If node <= -1, the target node if assumed to be the current NODE
+        from the last transaction through this FLOW.
 
-        What the current node needs is a "pointer" to the the flow and node the 
-        current node wanted to load the output of. The session token is accepted
-        in lieu of the transaction id.
+        If last_call is True, LoadInput() attempts to access the last output of 
+        the from_node of this FLOW.
 
-        The pointer will look like '<TXN>.<FLOW>.<NODE>:<KEY>[/<FIELD>]'. The 
-        scheduler will only confirm that there exists a valid session token 
-        matching the flow and node.        
+        In the future we'll add support for other FLOW
     """
-    
+    call:str="THIS" # for debug
+    session_token:str=CURRENT_TOKEN
+    if last_call:
+        session_token = FLOW_TOKEN
+        call = "LAST"        
+
+    if node < 0:
+        node = NODE
+
+    print("requesting output of node {} from {} call of flow {} with session token '{}'".format(
+        NODE, call, FLOW, session_token))
+
+    header = common_pb2.Header(Src="python_client", Dst=SCHEDULER_ADDR,
+                                SessionToken=session_token)
+
+    # call the Get method of the Scheduler services
+    response:common_pb2.SetResponse
+    with grpc.insecure_channel(SCHEDULER_ADDR) as channel:
+        # When no Pairs are set all variables are returned if the token is valid
+        stub = common_pb2_grpc.ScheduleStub(channel)
+        response = stub.Get(common_pb2.GetRequest(
+            Header=header,
+        ))
+        print("error:", response.Error, ", errMsg:",response.ErrorMsg)
+        if response.Error != common_pb2.ServiceError.SERVICE_ERROR_NONE:
+            return {}
+        
+        # parse values and add to run.args and run.kwargs\
     
 
 def InferType(s:str) -> (int|float|bool|str):
@@ -106,31 +180,68 @@ def InferType(s:str) -> (int|float|bool|str):
 
 
 def GetGlobal(key:str, infer_type=True) -> (int|float|bool|str):
-    """ returns a typed version of the global variable with the provided key.
+    """ returns the global variable with the key provided, if it exists.
+        By default GetGlobal returns a typed version of the value.
     """
     
 
-
+def LoadInput(values:dict[str,str]=None) -> tuple[list[str], dict[str,str]]|None:
+    # otherwise clear out any default values and populate from the provided dict
+    positional_dict:dict[int, str] = {}
+    remaining_kwargs = values.copy()
+    for k, v in values.items():
+        m = positionRe.match(k)
+        if m is not None:
+            positional_dict[int(m.group('position'))] = v
+            remaining_kwargs.pop(k)
+        # else:
+        #     print("'{}' did not match the positional argument pattern".format(k))
+    
+    args = [None] * len(positional_dict)
+    for i, v in positional_dict.items():
+        args[i-1] = v
+    return args, remaining_kwargs
 
 # container management functions 
-def LoadArgs():
-    # collect all the args
-    i = 1
-    while True:
-        try:
-            arg = os.environ.pop("arg:{}".format(i))
-            args.append(arg)
-            i += 1
-        except KeyError:
-            break
+def LoadArgs(values:dict[str,str]=None) -> list[str]|None:
+    if values is None:
+        # populate args from the OS environment
+        i = 1
+        while True:
+            try:
+                arg = os.environ.pop("arg:{}".format(i))
+                args.append(arg)
+                i += 1
+            except KeyError:
+                break
+        return
+        
 
-def LoadKwargs():
+def LoadKwargs(values:dict[str,str]=None):
     # collect all the args
     for k, v in os.environ.items():
         if "kwarg:" in k:
             kwargs[k[6:]] = os.environ.pop(k)
 
 def LoadEnv():
+    try:
+        TXN = os.environ.pop("TXN_ID")
+        TXN = int(TXN)
+    except KeyError:
+        TXN = 0
+        
+    try:
+        FLOW = os.environ.pop("FLOW_ID")
+        FLOW = int(FLOW)
+    except KeyError:
+        FLOW = 0
+
+    try:
+        NODE = os.environ.pop("NODE_ID")
+        NODE = int(NODE)
+    except KeyError:
+        NODE = 0        
+            
     LoadArgs()
     LoadKwargs()
 
--- src/bospy/run_test.py
+++ src/bospy/run_test.py
@@ -12,9 +12,68 @@ def TestInferType():
         print("'{}' is instance of {}".format(s, type(typed_str)))
         assert isinstance(typed_str, answers[i])
 
-def TestLoadInput():
- 
+def TestReturnValues():
+    """ demonstrates how to return numbered arguments and keyword arguments
+    """
+    args = [
+        "bos://localhost/dev/1/pts/1",
+        "bos://localhost/dev/1/pts/2",
+        "bos://localhost/dev/1/pts/3",
+    ]
+    kwargs = {
+        "times_accessed": 11,
+    }
 
+    resp = run.Return(*args, **kwargs)
+    print(resp)
+
+def TestLoadInput():
+    """ demonstrates how to load output from a previous node or instance of a flow.
+    """
+    test_cases = {
+        "$3": "baz",
+        "$2": "bar",
+        "$1": "foo",
+        "alice": "bob",
+    }
+
+    args, kwargs = run.LoadInput(test_cases)
+    print("args:  ", args)
+    print("kwargs:", kwargs)
+
+def TestMatchPositional():
+    test_cases = {
+        "$3": "baz",
+        "$2": "bar",
+        "$1": "foo",
+        "alice": "bob",
+    }
+
+    positional_dict:dict[int, str] = {}
+    for k, v in test_cases.items():
+        m = run.positionRe.match(k)
+        if m is not None:
+            positional_dict[int(m.group('position'))] = v
+        else:
+            print("'{}' did not match the positional argument pattern".format(k))
+        
+
+    args = [None] * len(positional_dict)
+    for i, v in positional_dict.items():
+        args[i-1] = v
+    
+    print("the ordered positional arguments are: {}".format(args))
+
+def TestGet():
+    keys = ["global:occupied",
+            "global:weekday_occupied_start",
+            "global:weekday_occupied_end",
+            "OUTPUT/times_accessed",
+            "an_invalid_token",
+            ]
+    results = run.Get(keys)
+    for k, v in results.items():
+        print(k, v)
 
 if __name__ == "__main__":
     # bospy.run.Run("random-get", "other-arg", envVars={"ENVVAR": "hello"}, anotherVar="hello again")
@@ -24,6 +83,10 @@ if __name__ == "__main__":
     # resp = bospy.run.Return("hello", True, 10, 100.1, name="James", age=30)
     # print(resp.ErrorMsg, resp.Error)
     # TestInferType()
+    # TestReturnValues()
+    # TestLoadInput()
+    # TestMatchPositional()
+    TestGet()
 
 
 
